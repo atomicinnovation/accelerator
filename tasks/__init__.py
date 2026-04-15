c0684d4ab67fd7acd1d42687c653d4465eb92fde
Toby Clemson <toby.clemson@ebury.com>
2026-04-15 22:27:20 +0100

    Add build system and pipeline to run tests.

---
+++ tasks/__init__.py
@@ -0,0 +1,9 @@
+from invoke import Collection
+
+from . import (
+    test,
+)
+
+ns = Collection()
+
+ns.add_collection(Collection.from_module(test))
---
+++ tasks/test.py
@@ -0,0 +1,12 @@
+from invoke import Context, task
+
+@task
+def integration(context: Context):
+    """Run integration tests."""
+    print("Running configuration script tests...")
+    context.run("scripts/test-config.sh")
+    print("\n")
+
+    print("Running ADR script tests...")
+    context.run("skills/decisions/scripts/test-adr-scripts.sh")
+    print("\n")
