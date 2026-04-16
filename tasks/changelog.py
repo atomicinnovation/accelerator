6be8e11c00d05d654e7c47b982ec2c00f8c896c2
Toby Clemson <toby.clemson@ebury.com>
2026-04-16 16:26:24 +0100

    Add release step to pipeline.

--- tasks/changelog.py
+++ tasks/changelog.py
@@ -1,6 +1,11 @@
+import keepachangelog
 from invoke import Context, task
 
+from tasks.version import read as version_read
+
+
 @task
-def release(_context: Context):
-    """Generate a changelog."""
-    pass
+def release(context: Context):
+    """Mark unreleased changelog entries with the current version."""
+    version = version_read(context, print_to_stdout=False)
+    keepachangelog.release("CHANGELOG.md", new_version=str(version))
--- tasks/release.py
+++ tasks/release.py
@@ -1,6 +1,6 @@
 from invoke import Context, task
 
-from . import version, git
+from . import changelog, git, version
 
 @task
 def prerelease(context: Context):
@@ -9,4 +9,21 @@ def prerelease(context: Context):
     version.bump(context, release_type="pre")
     git.tag_version(context)
     git.commit_version(context)
+    git.push(context)
+
+
+@task
+def release(context: Context):
+    """Prepare a release."""
+    git.configure(context)
+
+    version.bump(context, release_type="minor")
+    changelog.release(context)
+    git.tag_version(context)
+    git.commit_version(context)
+
+    version.bump(context, release_type="pre")
+    git.tag_version(context)
+    git.commit_version(context)
+
     git.push(context)
